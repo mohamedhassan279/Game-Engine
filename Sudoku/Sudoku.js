9a8664aaf7f06745398a482214f5df197292a0d7
Mohamed <mo.hassan2792@gmail.com>
2023-05-05 01:23:33 +0300

    sudoku random generatoe

--- Sudoku/Sudoku.js
+++ Sudoku/Sudoku.js
@@ -7,17 +7,137 @@ export class Sudoku extends GameEngine {
     }
 
     init() {
-        let state = [["05", "03", "", "", "07", "", "", "", ""],
-                     ["06", "", "", "01", "09", "05", "", "", ""],
-                     ["", "09", "08", "", "", "", "", "06", ""],
-                     ["08", "", "", "", "06", "", "", "", "03"],
-                     ["04", "", "", "08", "", "03", "", "", "01"],
-                     ["07", "", "", "", "02", "", "", "", "06"],
-                     ["", "06", "", "", "", "", "02", "08", ""],
-                     ["", "", "", "04", "01", "09", "", "", "05"],
-                     ["", "", "", "", "08", "", "", "07", "09"]];
-        
-        return [state, 0];
+        let state;
+        state = Array.from({
+            length: 9
+        }, () => Array.from({
+            length: 9
+        }, () => ""));
+        return [this.fillValues(state), 0];
+    }
+
+    fillValues(mat) {
+        // Fill the diagona3 matrices
+        this.fillDiagonal(mat);
+ 
+        // Fill remaining blocks
+        this.fillRemaining(0, 3, mat);
+ 
+        // Remove Randomly K digits to make game
+        this.removeKDigits(mat);
+        return mat;
+    }
+
+    fillDiagonal(mat) {
+        for (let i = 0; i < 9; i += 3) {
+            // for diagonal box, start coordinates->i==j
+            this.fillBox(i, i, mat);
+        }
+    }
+
+    unUsedInBox(rowStart, colStart, num, mat) {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (Number(mat[rowStart + i][colStart + j]) === num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    fillBox(row, col, mat) {
+        let num = 0;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                while (true) {
+                    num = this.randomGenerator(9);
+                    if (this.unUsedInBox(row, col, num, mat)) {
+                        break;
+                    }
+                }
+                mat[row + i][col + j] = '0' + String(num);
+            }
+        }
+    }
+
+    randomGenerator(num) {
+        return Math.floor(Math.random() * num + 1);
+    }
+
+    checkIfSafe(i, j, num, mat) {
+        return (
+            this.unUsedInRow(i, num, mat) &&
+            this.unUsedInCol(j, num, mat) &&
+            this.unUsedInBox(i - (i % 3), j - (j % 3), num, mat)
+        );
+    }
+
+    unUsedInRow(i, num, mat) {
+        for (let j = 0; j < 9; j++) {
+            if (Number(mat[i][j]) === num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    unUsedInCol(j, num, mat) {
+        for (let i = 0; i < 9; i++) {
+            if (Number(mat[i][j]) === num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    fillRemaining(i, j, mat) {
+        // Check if we have reached the end of the matrix
+        if (i === 9 - 1 && j === 9) {
+            return true;
+        }
+ 
+        // Move to the next row if we have reached the end of the current row
+        if (j === 9) {
+            i += 1;
+            j = 0;
+        }
+ 
+ 
+        // Skip cells that are already filled
+        if (mat[i][j] !== "") {
+            return this.fillRemaining(i, j + 1, mat);
+        }
+ 
+        // Try filling the current cell with a valid value
+        for (let num = 1; num <= 9; num++) {
+            if (this.checkIfSafe(i, j, num, mat)) {
+                mat[i][j] = '0' + String(num);
+                if (this.fillRemaining(i, j + 1, mat)) {
+                    return true;
+                }
+                mat[i][j] = "";
+            }
+        }
+ 
+        // No valid value was found, so backtrack
+        return false;
+    }
+
+    removeKDigits(mat) {
+        let count = 40;
+ 
+        while (count !== 0) {
+            // extract coordinates i and j
+            let i = Math.floor(Math.random() * 9);
+            let j = Math.floor(Math.random() * 9);
+            if (mat[i][j] !== "") {
+                count--;
+                mat[i][j] = "";
+            }
+        }
+ 
+        return;
     }
 
     drawer(state) {
